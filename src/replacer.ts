6bb536033952b023217b69b2a773e6840f0f58b9
Yusuke Wada <yusuke@kamawada.com>
2022-12-13 08:56:02 +0900

    fix(deno): update url to import

--- src/replacer.ts
+++ src/replacer.ts
@@ -5,5 +5,8 @@ makeThisModuleAnExecutableReplacer(async ({ parsedImportExportStatement, version
   if (parsedImportExportStatement.parsedArgument.nodeModuleName === 'toucan-js') {
     return `import Toucan from 'https://cdn.skypack.dev/toucan-js@${version}'`
   }
+  if (parsedImportExportStatement.parsedArgument.nodeModuleName === 'hono') {
+    return "import Toucan from 'https://deno.land/x/hono/mod.ts'"
+  }
   return undefined
 })
